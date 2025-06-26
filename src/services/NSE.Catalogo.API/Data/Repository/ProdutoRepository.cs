638c0dda52d4a4c73429ac380ad082febd03460d
Willian Barata <willianbarata@gmail.com>
2025-06-25 23:32:48 -0300

    criando IRepository, IUnitOfWork e Implementando

---
+++ src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _catalogoContext;
+
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+        public IUnitOfWork UnitOfWork => _catalogoContext;
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _catalogoContext.Produtos.FindAsync(id);
+        }
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+        }
+        public void Adicionar(Produto produto)
+        {
+            _catalogoContext.Produtos.Add(produto);
+        }
+        public void Atualizar(Produto produto)
+        {
+            _catalogoContext.Produtos.Update(produto);
+        }
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+    }
+}
